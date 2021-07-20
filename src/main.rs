9e7bd3936f3d14e5ae314c4516b7df4eb7899431
Milesq <wmilosz88@gmail.com>
2021-07-20 19:38:44 +0200

    feat: get branch names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let branches = Command::new("git")
+        .arg("branch")
+        .output()
+        .unwrap()
+        .stdout;
+
+    let branches = String::from_utf8(branches).unwrap();
+    let branches = parse_branches(branches);
+}
+
+fn parse_branches(branches: String) -> Vec<String> {
+    branches
+        .split("\n")
+        .map(|el| el
+            .trim()
+            .trim_start_matches("* ")
+            .to_string())
+        .filter(|el| !el.is_empty())
+        .collect()
 }
