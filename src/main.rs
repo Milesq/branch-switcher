b4bdeb597bcdafbfd7a5270e9a8ef4589ae1f7db
Milesq <wmilosz88@gmail.com>
2021-07-20 19:42:11 +0200

    feat: checkout to choosen branch

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use dialoguer::Select;
 
 fn main() {
     let branches = Command::new("git")
@@ -9,6 +10,17 @@ fn main() {
 
     let branches = String::from_utf8(branches).unwrap();
     let branches = parse_branches(branches);
+
+    let choosen_branch = Select::new().items(&branches).interact().unwrap();
+
+    let output = Command::new("git")
+        .arg("checkout")
+        .arg(&branches[choosen_branch])
+        .output()
+        .unwrap();
+
+    println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
 }
 
 fn parse_branches(branches: String) -> Vec<String> {
